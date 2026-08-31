cdf51e01189890ba8127e6cf70861bc57d879eeb
Himanshu-KRE <himanshu.nag@probestack.io>
2026-08-31 14:02:36 +0530

    Update generated SDK file: src/main/java/com/probestack/sdk/ServerConfiguration.java

--- src/main/java/com/probestack/sdk/ServerConfiguration.java
+++ src/main/java/com/probestack/sdk/ServerConfiguration.java
@@ -18,7 +18,7 @@ import java.util.Map;
 /**
  * Representing a Server configuration.
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:23:13.109827530Z[GMT]", comments = "Generator version: 7.9.0")public class ServerConfiguration {
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:29:58.652364882Z[GMT]", comments = "Generator version: 7.9.0")public class ServerConfiguration {
     public String URL;
     public String description;
     public Map<String, ServerVariable> variables;
