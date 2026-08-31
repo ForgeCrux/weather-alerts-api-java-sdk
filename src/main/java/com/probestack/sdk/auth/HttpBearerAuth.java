b49e9fcbcf19b23a7f2ea02ff3297930a2afb1fe
Himanshu-KRE <himanshu.nag@probestack.io>
2026-08-31 14:02:43 +0530

    Update generated SDK file: src/main/java/com/probestack/sdk/auth/HttpBearerAuth.java

--- src/main/java/com/probestack/sdk/auth/HttpBearerAuth.java
+++ src/main/java/com/probestack/sdk/auth/HttpBearerAuth.java
@@ -18,7 +18,7 @@ import java.util.function.Supplier;
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.MultiValueMap;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:23:13.109827530Z[GMT]", comments = "Generator version: 7.9.0")public class HttpBearerAuth implements Authentication {
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:29:58.652364882Z[GMT]", comments = "Generator version: 7.9.0")public class HttpBearerAuth implements Authentication {
     private final String scheme;
     private Supplier<String> tokenSupplier;
 
