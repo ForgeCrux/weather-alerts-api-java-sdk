91d7838d6d171e752b3f388e9fcb2d07bd9fcc51
Himanshu-KRE <himanshu.nag@probestack.io>
2026-08-31 14:02:40 +0530

    Update generated SDK file: src/main/java/com/probestack/sdk/api/DefaultApi.java

--- src/main/java/com/probestack/sdk/api/DefaultApi.java
+++ src/main/java/com/probestack/sdk/api/DefaultApi.java
@@ -31,7 +31,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:23:13.109827530Z[GMT]", comments = "Generator version: 7.9.0")public class DefaultApi extends BaseApi {
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:29:58.652364882Z[GMT]", comments = "Generator version: 7.9.0")public class DefaultApi extends BaseApi {
 
     public DefaultApi() {
         super(new ApiClient());
