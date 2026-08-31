72621536cdfb4bfc22cac24245c0a2a1ad24415f
Himanshu-KRE <himanshu.nag@probestack.io>
2026-08-31 14:02:41 +0530

    Update generated SDK file: src/main/java/com/probestack/sdk/JavaTimeFormatter.java

--- src/main/java/com/probestack/sdk/JavaTimeFormatter.java
+++ src/main/java/com/probestack/sdk/JavaTimeFormatter.java
@@ -20,7 +20,7 @@ import java.time.format.DateTimeParseException;
  * Class that add parsing/formatting support for Java 8+ {@code OffsetDateTime} class.
  * It's generated for java clients when {@code AbstractJavaCodegen#dateLibrary} specified as {@code java8}.
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:23:13.109827530Z[GMT]", comments = "Generator version: 7.9.0")public class JavaTimeFormatter {
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:29:58.652364882Z[GMT]", comments = "Generator version: 7.9.0")public class JavaTimeFormatter {
 
     private DateTimeFormatter offsetDateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
