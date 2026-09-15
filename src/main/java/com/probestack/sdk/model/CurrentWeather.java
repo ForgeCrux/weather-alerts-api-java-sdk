313b3a1c702a8ec9b6ebdcfd37730e1aa96e7558
Himanshu-KRE <himanshu.nag@probestack.io>
2026-09-15 23:53:37 +0530

    Update generated SDK file: src/main/java/com/probestack/sdk/model/CurrentWeather.java

--- src/main/java/com/probestack/sdk/model/CurrentWeather.java
+++ src/main/java/com/probestack/sdk/model/CurrentWeather.java
@@ -36,7 +36,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
   CurrentWeather.JSON_PROPERTY_HUMIDITY_PERCENT,
   CurrentWeather.JSON_PROPERTY_OBSERVED_AT
 })
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:35:58.204314211Z[GMT]", comments = "Generator version: 7.9.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-09-03T07:38:01.001853780Z[GMT]", comments = "Generator version: 7.9.0")
 public class CurrentWeather {
   public static final String JSON_PROPERTY_LOCATION = "location";
   private String location;
