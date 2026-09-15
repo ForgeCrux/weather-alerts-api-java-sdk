ab2cdffc2f36e11d72439a8d418ce999884c23a7
Himanshu-KRE <himanshu.nag@probestack.io>
2026-09-15 23:53:33 +0530

    Update generated SDK file: src/main/java/com/probestack/sdk/model/ForecastDay.java

--- src/main/java/com/probestack/sdk/model/ForecastDay.java
+++ src/main/java/com/probestack/sdk/model/ForecastDay.java
@@ -35,7 +35,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
   ForecastDay.JSON_PROPERTY_CONDITION,
   ForecastDay.JSON_PROPERTY_PRECIPITATION_CHANCE_PERCENT
 })
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:35:58.204314211Z[GMT]", comments = "Generator version: 7.9.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-09-03T07:38:01.001853780Z[GMT]", comments = "Generator version: 7.9.0")
 public class ForecastDay {
   public static final String JSON_PROPERTY_DATE = "date";
   private LocalDate date;
