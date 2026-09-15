c2087216a44630b6ee039806d3f1aa162c2e84a1
Himanshu-KRE <himanshu.nag@probestack.io>
2026-09-15 23:53:34 +0530

    Update generated SDK file: src/main/java/com/probestack/sdk/model/SubscribeToAlertsRequest.java

--- src/main/java/com/probestack/sdk/model/SubscribeToAlertsRequest.java
+++ src/main/java/com/probestack/sdk/model/SubscribeToAlertsRequest.java
@@ -32,7 +32,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
   SubscribeToAlertsRequest.JSON_PROPERTY_MIN_SEVERITY
 })
 @JsonTypeName("subscribeToAlerts_request")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-31T08:35:58.204314211Z[GMT]", comments = "Generator version: 7.9.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-09-03T07:38:01.001853780Z[GMT]", comments = "Generator version: 7.9.0")
 public class SubscribeToAlertsRequest {
   public static final String JSON_PROPERTY_EMAIL = "email";
   private String email;
